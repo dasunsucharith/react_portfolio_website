e89d36a34e16518fafa750b2c363ef0931980bf3
Dasun Sucharith <sucharith.dasun@gmail.com>
2022-04-25 06:32:50 +0530

    Update Experience.jsx

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -5,6 +5,12 @@ function Experience() {
     return (
         <section id="experience">
             <h5>The Skills I have</h5>
+            <h2>My Experience</h2>
+
+            <div className="container experience__container">
+                <div className="experience__frontend"></div>
+                <div className="experience__backend"></div>
+            </div>
         </section>
     )
 }
